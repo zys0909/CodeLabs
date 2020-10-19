21cda95c1d08743995d054416111a46894cacfbb
zhaoys <zhaoys@centaline.com.cn>
2020-10-19 17:55:11 +0800

    1

---
+++ src/com/leecode/Code2.kt
@@ -0,0 +1,68 @@
+package com.leecode
+
+/**
+ * 找出数组中重复的数字。
+
+
+在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+
+示例 1：
+
+输入：
+[2, 3, 1, 0, 2, 5, 3]
+输出：2 或 3
+ 
+
+限制：
+
+2 <= n <= 100000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof
+ */
+
+/**
+ * 用一个容器装已经确定的数
+ */
+fun findRepeatNumber1(nums: IntArray): Int {
+    val map = mutableSetOf<Int>()
+    for (i in nums) {
+        if (map.contains(i)) {
+            return i
+        } else {
+            map.add(i)
+        }
+    }
+    return -1
+}
+
+/**
+ * 先排序，再判断相邻的数是否相等
+ */
+fun findRepeatNumber2(nums: IntArray): Int {
+    nums.sort()
+    for (i in 1 until nums.size) {
+        if (nums[i] == nums[i - 1]) {
+            return nums[i]
+        }
+    }
+    return -1
+}
+
+/**
+ * 所有数字都在长度范围内，将每个数字放到对应的下标位置，判断当前数字是否与该位置的数字相等
+ */
+fun findRepeatNumber3(nums: IntArray): Int {
+    for (i in nums.indices) {
+        val n = nums[i]
+        if (i != n) {
+            if (nums[n] == n) {
+                return n
+            } else {
+                nums[i] = nums[n]
+                nums[n] = n
+            }
+        }
+    }
+    return -1
+}
---
+++ src/com/leecode/Code3.kt
@@ -0,0 +1,79 @@
+package com.leecode
+
+/**
+输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+
+ 
+
+示例：
+
+输入：nums = [1,2,3,4]
+输出：[1,3,2,4]
+注：[3,1,2,4] 也是正确的答案之一。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+ */
+/**
+ * 申请一个新数组，T(n) ,O(n)
+ */
+fun exchange1(nums: IntArray): IntArray {
+    var start = 0
+    var end = nums.size - 1
+    val temp = IntArray(nums.size)
+    for (i in nums.indices) {
+        if (nums[i].and(1) == 1) {
+            temp[start++] = nums[i]
+        } else {
+            temp[end--] = nums[i]
+        }
+    }
+    return temp
+}
+
+/**
+ * 双指针法，T(n2),O(1)
+ * start 从前往后找第一个偶数，end从后往前找最后一个奇数，找到后交换位置
+ */
+fun exchange2(nums: IntArray): IntArray {
+    var start = 0
+    var end = nums.size - 1
+    var temp: Int
+    while (start < end) {
+        while (start < end && nums[start].and(1) == 1) {
+            start++
+        }
+        while (start < end && nums[end].and(1) == 0) {
+            end--
+        }
+        if (start < end) {
+            temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+        }
+    }
+    return nums
+}
+
+/**
+ * 快慢指针
+ */
+fun exchange3(nums: IntArray): IntArray {
+    var i = 0
+    var j = 0
+    var temp: Int
+    while (i < nums.size) {
+        if (nums[i].and(1) == 1) {
+            if (i != j) {
+                temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+            }
+            j++
+        }
+        i++
+    }
+    return nums
+}
